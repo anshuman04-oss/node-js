cbb071bf8016c81f2ccf66901fa399f5cd42c7dd
anshuman04-oss <acharyaanshuman04.com>
2025-05-17 15:55:19 +0530

    Just another commit with minor changes

--- restAPIs/index.js
+++ restAPIs/index.js
@@ -9,20 +9,28 @@ app.get("/api/users", (req, res) => {
 app.get("/users", (req, res) => {
   const html = `
   <ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`)}
+    ${users.map((user) => `<li>${user.first_name}</li>`).join(" ")}
   </ul>
   `;
   res.send(html);
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const id = req.params.id;
-  const user = users.find((user) => user.id === id);
-  if(user) {
-    return res.json(user);
-  } else {
-    return res.status(404).json("User not found");
-  }
-});
+app.route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    if(user) {
+      return res.json(user);
+    } else {
+      return res.status(404).json("User not found");
+    }
+  })
+  .put((req, res) => {
+    return res.status({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    return res.status({ status: "Pending" });
+  })
+
 
 app.listen(8000, () => console.log("The server is listening on port 8000"));
